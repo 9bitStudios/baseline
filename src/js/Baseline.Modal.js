ff409b6d7634673e153d8e0c1534786ff7ec1fbf
9bit Studios <9bitStudios@gmail.com>
2017-04-01 10:02:15 -0700

    Adding CSS and JavaScript for modal dialogs... Adding "jQuery" dependency to package.json...

---
+++ src/js/Baseline.Modal.js
@@ -0,0 +1,68 @@
+import $ from 'jquery';
+import {Utilities} from './Baseline.Utilities';
+
+export class Modal {
+    
+    constructor(selector){
+        this.selector = selector;
+        this.addBackground();
+    }
+
+    addBackground(){
+		// if this has already been added we don't need to add it again
+		if ($('.baseline-modal-background').length === 0) {
+		    let background = '<div class="baseline-modal-background"></div>';
+		    $('body').prepend(background);
+            this.setEventHandlers();
+		}        
+    }
+
+    setEventHandlers(){
+
+		$('.baseline-modal-background').on("click", () => this.hide());
+
+        let timeout;
+        
+		$(window).on("resize", function(event){
+            clearTimeout(timeout);
+            timeout = setTimeout(function(){
+                this.positionModal();
+            });
+		});				
+       
+    }
+
+    removeEventHandlers(){
+        $(".baseline-modal-background").off("click"); 
+    }
+
+    show(){
+        $(".baseline-modal-background").css({ "opacity": "0.7" }).fadeIn("fast");
+        $(this.selector).fadeIn("slow");
+        this.positionModal();
+    }
+
+    hide(){
+        $(".baseline-modal-background").fadeOut("fast");
+        $(this.selector).fadeOut("fast");
+    }
+
+    positionModal(){
+
+        var object = $(this.selector);
+		var windowWidth = $(window).width();
+		var windowHeight = $(window).height();
+		var popupWidth = object.width();				
+		var popupHeight = object.height();
+
+		var topPos = (windowHeight / 2) - (popupHeight / 2);
+		var leftPos = (windowWidth / 2) - (popupWidth / 2);
+		if(topPos < 30) topPos = 30;
+		
+		object.css({
+		    "top": topPos,
+		    "left": leftPos
+		});
+    }
+
+}
--- src/js/Baseline.js
+++ src/js/Baseline.js
@@ -1,9 +1,11 @@
 import {Utilities} from './Baseline.Utilities';
+import {Modal} from './Baseline.Modal';
 
 class Baseline {
     
     constructor(){
         this.Utilities = Utilities;
+        this.Modal = Modal;
     }
 }
 
